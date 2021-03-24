2124d06528e116794b9fcc928179b87592607d5b
thanawat-bcr <thanawat.bcr@gmail.com>
2021-03-24 19:39:59 +0700

    [ADD] git ignore

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ import express from 'express';
 
 const mongoose = require('mongoose');
 mongoose.connect(
-  //   'mongodb://172.16.167.2:27017/test',
   'mongodb://tutorism:1234@172.16.167.2:27017/test?authSource=admin',
-  //   'mongodb://127.0.0.1:27017/myDB',
+  // 'mongodb://tutorism:1234@127.0.0.1:27017/test?authSource=admin',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
