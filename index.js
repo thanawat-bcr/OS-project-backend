0b84e8ae9d8913b027ca24e725a450675e9f79cd
thanawat-bcr <thanawat.bcr@gmail.com>
2021-03-29 22:17:32 +0700

    [UPDATE] MQTT local

--- index.js
+++ index.js
@@ -1,85 +1,116 @@
 import express from 'express';
 
-const mongoose = require('mongoose');
-mongoose.connect(
-  'mongodb://tutorism:1234@172.16.167.2:27017/test?authSource=admin',
-  // 'mongodb://tutorism:1234@127.0.0.1:27017/test?authSource=admin',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// mqtt connection
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://127.0.0.1:1883', {
+  username: 'tutorism',
+  password: '1234',
+});
+// var client = mqtt.connect('mqtt://172.16.167.2:1883', {
+//   username: 'tutorism',
+//   password: '1234',
+// });
+console.log('connected  ' + client.connected);
 
-var PORT = process.env.PORT || 80;
+client.on('connect', function () {
+  console.log('connected  ' + client.connected);
+  client.publish('test', 'connected');
+});
+
+client.on('error', function (error) {
+  console.log("Can't connect" + error);
+});
+
+// // mongoose connection
+// const mongoose = require('mongoose');
+// mongoose.connect(
+//   'mongodb://tutorism:1234@172.16.167.2:27017/test?authSource=admin',
+//   // 'mongodb://tutorism:1234@127.0.0.1:27017/test?authSource=admin',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
+
+var PORT = process.env.PORT || 3000;
 var app = express();
 app.listen(PORT);
 
-// DB connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-  console.log('DB connected');
-});
+// // DB connection
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function () {
+//   // we're connected!
+//   console.log('DB connected');
+// });
 
-// Test schema
-const productSchema = mongoose.Schema({
-  name: String,
-  price: Number,
-});
-const Product = mongoose.model('Product', productSchema);
+// // Test schema
+// const productSchema = mongoose.Schema({
+//   name: String,
+//   price: Number,
+// });
+// const Product = mongoose.model('Product', productSchema);
 
-const userSchema = mongoose.Schema(
-  {
-    name: String,
-    age: Number,
-  },
-  { collection: 'user' }
-);
-const User = mongoose.model('User', userSchema);
+// const userSchema = mongoose.Schema(
+//   {
+//     name: String,
+//     age: Number,
+//   },
+//   { collection: 'user' }
+// );
+// const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
   return res.status(200).send('hello');
 });
 
-app.get('/test', (req, res) => {
-  const product = new Product({
-    name: 'Tutor',
-    price: 1000,
-  });
-  product
-    .save()
-    .then((doc) => console.log(doc))
-    .catch((err) => console.log(err));
-  return res.status(200).send('hello');
+app.get('/open', (req, res) => {
+  client.publish('test', 'open');
+  return res.status(200).send('open');
+});
+app.get('/close', (req, res) => {
+  client.publish('test', 'close');
+  return res.status(200).send('close');
 });
 
-app.get('/insert/user', (req, res) => {
-  const user = new User({
-    name: 'No Collection',
-    age: 20,
-  });
+// app.get('/test', (req, res) => {
+//   const product = new Product({
+//     name: 'Tutor',
+//     price: 1000,
+//   });
+//   product
+//     .save()
+//     .then((doc) => console.log(doc))
+//     .catch((err) => console.log(err));
+//   return res.status(200).send('hello');
+// });
 
-  user
-    .save()
-    .then((doc) => {
-      console.log(doc);
-      return res.status(200).send('hello');
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(400).send('hello');
-    });
-});
+// app.get('/insert/user', (req, res) => {
+//   const user = new User({
+//     name: 'No Collection',
+//     age: 20,
+//   });
 
-app.get('/find/user', (req, res) => {
-  User.find()
-    .then((doc) => {
-      console.log(doc);
-      return res.status(200).json({ log: doc });
-    })
-    .catch((err) => {
-      console.log(err);
-      return res.status(401).json({ log: 'success' });
-    });
-});
+//   user
+//     .save()
+//     .then((doc) => {
+//       console.log(doc);
+//       return res.status(200).send('hello');
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       return res.status(400).send('hello');
+//     });
+// });
+
+// app.get('/find/user', (req, res) => {
+//   User.find()
+//     .then((doc) => {
+//       console.log(doc);
+//       return res.status(200).json({ log: doc });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       return res.status(401).json({ log: 'success' });
+//     });
+// });
