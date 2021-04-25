c76ad965828b8e763748363ab4981db4e54518b1
thanawat-bcr <thanawat.bcr@gmail.com>
2021-04-25 22:04:25 +0700

    [ADD] CRUD

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 var PORT = process.env.PORT || 3000;
 var app = express();
+app.use(cors());
 app.listen(PORT);
 
 // mqtt connection
@@ -10,7 +12,7 @@ var client = mqtt.connect('mqtt://127.0.0.1:1883', {
   username: 'tutorism',
   password: '1234',
 });
-// var client = mqtt.connect('mqtt://172.16.167.2:1883', {
+// var client = mqtt.connect('mqtt://192.168.1.37:1883', {
 //   username: 'tutorism',
 //   password: '1234',
 // });
@@ -28,7 +30,7 @@ client.on('error', function (error) {
 // mongoose connection
 const mongoose = require('mongoose');
 mongoose.connect(
-  // 'mongodb://tutorism:1234@172.16.167.2:27017/temp?authSource=admin',
+  // 'mongodb://tutorism:1234@192.168.1.37:27017/data?authSource=admin',
   'mongodb://tutorism:1234@127.0.0.1:27017/data?authSource=admin',
   {
     useNewUrlParser: true,
@@ -44,26 +46,16 @@ db.once('open', function () {
   console.log('DB connected');
 });
 
-// // Test schema
-// const productSchema = mongoose.Schema({
-//   name: String,
-//   price: Number,
-// });
-// const Product = mongoose.model('Product', productSchema);
-
-// const userSchema = mongoose.Schema(
-//   {
-//     name: String,
-//     age: Number,
-//   },
-//   { collection: 'user' }
-// );
-// const User = mongoose.model('User', userSchema);
-
 const tempSchema = mongoose.Schema(
   {
     temp: Number,
-    timestamp: String,
+    day: String,
+    date: String,
+    month: String,
+    year: String,
+    hour: String,
+    minute: String,
+    second: String,
   },
   { collection: 'temp' }
 );
@@ -72,22 +64,23 @@ const Temp = mongoose.model('Temp', tempSchema);
 app.get('/', (req, res) => {
   return res.status(200).send('hello');
 });
-// app.get('/open', (req, res) => {
-//   client.publish('test', 'open');
-//   return res.status(200).send('open');
-// });
-// app.get('/close', (req, res) => {
-//   client.publish('test', 'close');
-//   return res.status(200).send('close');
-// });
-// app.get('/test', (req, res) => {
-//   client.publish('test', 'Hello World');
-//   return res.status(200).send('success');
-// });
+
 app.get('/temp', (req, res) => {
   // Define data
   const { temp } = req.query;
   const timestamp = '' + new Date(Date.now()).toString().split('G')[0];
+  const tmp = timestamp.split(' ');
+  const day = tmp[0];
+  const times = tmp[4].split(':');
+  const hour = times[0];
+  const minute = times[1];
+  const second = times[2];
+
+  const now = new Date();
+  const year = now.getFullYear();
+  const month =
+    now.getMonth() + 1 < 10 ? '0' + (now.getMonth() + 1) : now.getMonth() + 1;
+  const date = now.getDate();
 
   // Publish to frontend
   client.publish('temp', '' + temp);
@@ -95,9 +88,16 @@ app.get('/temp', (req, res) => {
 
   // Write on DB
   const tempItem = new Temp({
-    temp: temp,
-    timestamp: timestamp,
+    temp,
+    day,
+    date,
+    month,
+    year,
+    hour,
+    minute,
+    second,
   });
+
   tempItem
     .save()
     .then((doc) => console.log(doc))
@@ -105,44 +105,58 @@ app.get('/temp', (req, res) => {
   return res.status(200).send('success');
 });
 
-// app.get('/test', (req, res) => {
-//   const product = new Product({
-//     name: 'Tutor',
-//     price: 1000,
-//   });
-//   product
-//     .save()
-//     .then((doc) => console.log(doc))
-//     .catch((err) => console.log(err));
-//   return res.status(200).send('hello');
-// });
+app.get('/temp/date', (req, res) => {
+  const { date, month, year } = req.query;
+  Temp.find({ date: date, month: month, year: year })
+    .then((doc) => {
+      console.log(doc);
+      return res.status(200).json({ log: doc });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(401).json({ log: 'success' });
+    });
+});
 
-// app.get('/insert/user', (req, res) => {
-//   const user = new User({
-//     name: 'No Collection',
-//     age: 20,
-//   });
+app.get('/temp/temp', (req, res) => {
+  const { temp } = req.query;
+  Temp.find({ temp: temp })
+    .then((doc) => {
+      console.log(doc);
+      return res.status(200).json({ log: doc });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(401).json({ log: 'success' });
+    });
+});
 
-//   user
-//     .save()
-//     .then((doc) => {
-//       console.log(doc);
-//       return res.status(200).send('hello');
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       return res.status(400).send('hello');
-//     });
-// });
+app.get('/temp/remove', (req, res) => {
+  const { id } = req.query;
+  console.log(id);
 
-// app.get('/find/user', (req, res) => {
-//   User.find()
-//     .then((doc) => {
-//       console.log(doc);
-//       return res.status(200).json({ log: doc });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       return res.status(401).json({ log: 'success' });
-//     });
-// });
+  Temp.remove({ _id: id })
+    .then((doc) => {
+      console.log(doc);
+      return res.status(200).json({ log: doc });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(401).json({ log: 'success' });
+    });
+});
+app.get('/temp/update', (req, res) => {
+  const { id, temp, date, month, year, hour, minute, second } = req.query;
+  Temp.findOneAndUpdate(
+    { _id: id },
+    { temp, date, month, year, hour, minute, second }
+  )
+    .then((doc) => {
+      console.log(doc);
+      return res.status(200).json({ log: doc });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(401).json({ log: 'success' });
+    });
+});
